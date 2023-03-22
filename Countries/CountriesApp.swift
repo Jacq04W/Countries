7783c2046c72846361a3845d5eea9ec86865b4fd
Jacquese <jacqwhitson@gmail.com>
2023-03-22 15:03:01 -0400

    Initial Commit

---
+++ Countries/CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesApp.swift
+//  Countries
+//
+//  Created by Jacquese Whitson  on 3/22/23.
+//
+
+import SwiftUI
+
+@main
+struct CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
